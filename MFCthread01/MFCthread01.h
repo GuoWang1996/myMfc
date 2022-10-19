f89ba940d1525cbb29fbe6c6f3d556af55916fa8
wang guo <2431605485@qq.com>
2022-10-19 10:14:28 +0800

    多线程项目创建

---
+++ MFCthread01/MFCthread01.h
@@ -0,0 +1,32 @@
+﻿
+// MFCthread01.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCthread01App:
+// 有关此类的实现，请参阅 MFCthread01.cpp
+//
+
+class CMFCthread01App : public CWinApp
+{
+public:
+	CMFCthread01App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCthread01App theApp;
