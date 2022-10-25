3872f110920c34f813f194d5d2ad29a9ac699576
wang guo <2431605485@qq.com>
2022-10-25 10:45:29 +0800

    156.枚举当前所有顶级窗口和实战小工具

---
+++ MFCthread06/MFCthread06.h
@@ -0,0 +1,32 @@
+﻿
+// MFCthread06.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMFCthread06App:
+// 有关此类的实现，请参阅 MFCthread06.cpp
+//
+
+class CMFCthread06App : public CWinApp
+{
+public:
+	CMFCthread06App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCthread06App theApp;
